125466da81be29633308d6ef11671787913f7ecf
Akash <akashchauhan84590@gmail.com>
2023-09-24 12:08:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+// console.log("This is mew");
+// document.write("This is Document")
+
+
+
+//java script Variables
+
+// var number1 = 34;
+// var number2 = 32;
+// console.log(number1 + number2)
+
+
+// var number1 = 34;
+// var number2 = 32;
+// console.log(number1 * number2)
+
+// 4. data Types In javaScript
+
+// var str1 = "This Is a String"
+// var str1 = 'Strings';
+
+
+//objexts
+
+
+// var marks = {
+//     ravi : 23,
+//     piyu: 34,
+//     LDS:9999
+
+// }
+
+// Undefined variable
+
+// var und = undefined;
+// console.log(undefined);
+
+// var und = null
+// console.log(null);
+
+
+
+// var age = 23;
+// if (age > 3)
+// {
+//     console.log('You are not a kid')
+// }
+
+
+
+// let elem = document.getElementById('click');
+// console.log(elem);
+
+// let elemclass = document.getElementsByClassName("container") 
+// console.log(elemclass);
+// elemclass[0].style.background = "yellow" 
+// elemclass[1].style.background = "pink" 
+
+
